90c339ae961fbb326de850037b6d8f707e8d84af
Eric Sun <ergang.sun@gmail.com>
2014-03-20 18:49:34 -0700

    try posting to tag

---
+++ app/models/tag.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose')
+  // , Imager = require('imager')
+  // , env = process.env.NODE_ENV || 'development'
+  // , config = require('../../config/config')[env]
+  // , imagerConfig = require(config.root + '/config/imager.js')
+  , Schema = mongoose.Schema;
+
+  
--- server.js
+++ server.js
@@ -1,9 +1,59 @@
 var express = require('express');
 var app = express();
 
-app.get('/location_attr', function(req, res){
-  res.send("what's up iBeacon people");
+var mongoose = request('mongoose');
+mongoose.connect('mongodb://localhost/beacon_db');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+
 });
+
+var tagSchema = mongoose.Schema({
+	business: String,
+    name : String,
+    beacons: [{
+    	uuid: String,
+    	major: String,
+    	minor: String,
+    	distance: Number,
+    	rssi: Number
+    }]
+});
+
+var Tag = moongose.model('Tag', tagSchema);
+
+app.post('/tags', function(req, res)) {
+	if(req && req.body) {
+		var tag = new Tag({
+			business : req.body.business,
+			name : req.body.name
+		});
+
+		if(request.body.beacons && req.body.beacons.length > 0) {
+			tag.beacons = [];
+			req.body.beacons.forEach(function(beacon) {
+				tag.beacons.push[beacon];
+			});
+		}
+		tag.save(function(err, t) {
+			if(err) {
+				res.json(499, {responseCode: 'CANNOT_SAVE_TO_DB', responseMessage: err});
+				return;
+			}
+			
+			res.json(t);
+			
+		});
+	}	
+
+});
+
+app.get('/tags', function(req, res){
+  
+});
+
 
 var server = app.listen(80, function() {
     console.log('Listening on port %d', server.address().port);
